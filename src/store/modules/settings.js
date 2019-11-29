4b52bb7ca4e9172abed41a3a3bd09d01c687a884
Simon Thiboutôt <simonthiboutot@hotmail.com>
2019-11-29 12:15:35 -0500

    Fix bug when reste default settings.

--- src/store/modules/settings.js
+++ src/store/modules/settings.js
@@ -56,7 +56,7 @@ export default {
         })
     },
     settingsSaveDefaultSettings ({ dispatch }) {
-      return dispatch('settingsSave', defaultSettings)
+      return dispatch('settingsSave', { ...defaultSettings })
     },
     settingsSave ({ state, commit }, settings) {
       commit('settingsSetSaveStatus', 'saving')
