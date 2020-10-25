594b56c893a628abeec7583846a3dd09b5f7c6a4
Simon Thiboutôt <simonthiboutot@hotmail.com>
2020-10-25 14:13:15 -0400

    Add component Status.

---
+++ src/components/Status.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="status" :class="{ [`status_type-${type}`]: true }" />
+</template>
+
+<script>
+export const DEFAULT_TYPE = 'empty'
+export const TYPES = ['empty', 'primary', 'secondary', 'accent', 'danger', 'success', 'warning', 'info']
+
+export default {
+  name: 'status',
+  props: {
+    type: {
+      type: String,
+      default: DEFAULT_TYPE,
+      validator: (value) => TYPES.includes(value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/stylesheets/constants.scss';
+@use 'sass:color';
+
+$opacity: 25%;
+
+.status {
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 100%;
+  border-width: 1px;
+  border-style: solid;
+
+  &_type-empty {
+    background: $color-background;
+    border-color: $font-color;
+  }
+  &_type-primary {
+    background: $color-primary;
+    border-color: darken($color-primary, $opacity);
+  }
+  &_type-secondary {
+    background: $color-secondary;
+    border-color: darken($color-secondary, $opacity);
+  }
+  &_type-accent {
+    background: $color-accent;
+    border-color: darken($color-accent, $opacity);
+  }
+  &_type-danger {
+    background: $color-danger;
+    border-color: darken($color-danger, $opacity);
+  }
+  &_type-success {
+    background: $color-success;
+    border-color: darken($color-success, $opacity);
+  }
+  &_type-warning {
+    background: $color-warning;
+    border-color: darken($color-warning, $opacity);
+  }
+  &_type-info {
+    background: $color-info;
+    border-color: darken($color-info, $opacity);
+  }
+
+}
+</style>
